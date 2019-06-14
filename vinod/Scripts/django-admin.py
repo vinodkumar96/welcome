205ef7bd6f316efd0d698a5a028cd760c4299b29
vinodkumar96 <vinodkumaryv96@gmail.com>
2019-06-14 19:23:48 +0530

    Initial commit

---
+++ vinod/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\PyCharm\PycharmProjects\new_projects\welcome\vinod\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
